17bedf6d9d192a2f0a96da73f0f017a974029be4
Kaz. HIDA <kazhida@abplus.com>
2012-12-02 14:36:47 +0900

    とりあえずrails new して、faviconを置いただけ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Famn::Application.config.secret_token = 'ceeabd500443f18fcc8ffca45aa4deec97a64efa3382700c546683799d40ba7e5607a5a1bdbabdf0a76af2e68d888e4c72f33927a21861ada39833e08842cfe9'
