c29c13cf9894e4f94b936def55eb15f7de4cbd12
kazhida <kazhida@abplus.com>
2012-12-09 12:53:49 +0900

    ユーザーのテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
-class User < ActiveRecord::Base
-  attr_accessible :login_name, :display_name, :password_digest, :mail_address, :aruji
 
-  validates :login_name, :presence => true
-  validates :display_name, :presence => true
-  validates :password_digest, :presence => true
-  validates :mail_address, :presence => true
-  validates :aruji, :presence => true
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :login_name, :display_name, :password, :mail_address, :aruji
+
+  validates_presence_of :login_name
+  validates_presence_of :display_name
+  validates_presence_of :password_digest
+  validates_presence_of :mail_address
+  validates_presence_of :aruji
+
+  validates_uniqueness_of :login_name
+
+  validates_length_of :password, {minimum: 6, allow_blank: true}
+
+  before_validation do
+    self.password_digest = BCrypt::Password.create(self.password)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,33 +19,114 @@ describe User do
     @user = User.new
   end
 
-  it 'login_nameがなければ保存できない。' do
-    @user.save.should be_false
+  describe 'validation' do
+
+    it 'login_nameがなければ保存できないはず' do
+      @user.save.should be_false
+    end
+
+    it 'display_nameがなければ保存できないはず' do
+      @user.login_name = 'foo'
+      @user.save.should be_false
+    end
+
+    it 'password_digestがなければ保存できないはず' do
+      @user.login_name = 'foo'
+      @user.display_name = 'Foo'
+      @user.save.should be_false
+    end
+
+    it 'mail_addressがなければ保存できないはず' do
+      @user.login_name = 'foo'
+      @user.display_name = 'Foo'
+      @user.password_digest = 'bar'
+      @user.save.should be_false
+    end
+
+    it 'arujiがなければ保存できないはず' do
+      @user.login_name = 'foo'
+      @user.display_name = 'Foo'
+      @user.password_digest = 'bar'
+      @user.mail_address = 'foo@example.com'
+      @user.save.should be_false
+    end
+
+    it 'passwordは6文字以上のはず' do
+      @user.attributes = {
+          login_name: 'foo',
+          display_name: 'Foo',
+          password: 'barbe',
+          mail_address: 'foo@example.com',
+          aruji: true
+      }
+      @user.save.should be_false
+    end
+
+    it 'passwordは6文字以上のはず' do
+      @user.attributes = {
+          login_name: 'foo',
+          display_name: 'Foo',
+          password: 'barboo',
+          mail_address: 'foo@example.com',
+          aruji: true
+      }
+      @user.save.should be_true
+    end
+
+    it 'login_nameはユニークなはず' do
+      User.new({
+          login_name: 'foo',
+          display_name: 'Foo',
+          password: 'barboo',
+          mail_address: 'foo@example.com',
+          aruji: true
+      }).save
+      @user.attributes = {
+          login_name: 'foo',
+          display_name: 'Foo2',
+          password: 'barboo',
+          mail_address: 'foo@example.com',
+          aruji: true
+      }
+      @user.save.should be_false
+    end
   end
 
-  it 'display_nameがなければ保存できない。' do
-    @user.login_name = 'foo'
-    @user.save.should be_false
-  end
+  describe 'accessible' do
 
-  it 'password_digestがなければ保存できない。' do
-    @user.login_name = 'foo'
-    @user.display_name = 'Foo'
-    @user.save.should be_false
-  end
+    it 'password_digestはmath_accessibleではないはず' do
+      lambda {
+        @user.attributes = {
+            login_name: 'foo',
+            display_name: 'Foo',
+            password_digest: 'bar',
+            mail_address: 'foo@example.com',
+            aruji: true
+        }
+      }.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
 
-  it 'mail_addressがなければ保存できない。' do
-    @user.login_name = 'foo'
-    @user.display_name = 'Foo'
-    @user.password_digest = 'bar'
-    @user.save.should be_false
-  end
+    it 'passwordはmass_accessibleのはず' do
+      lambda {
+        @user.attributes = {
+            login_name: 'foo',
+            display_name: 'Foo',
+            password: 'bar',
+            mail_address: 'foo@example.com',
+            aruji: true
+        }
+      }.should_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
 
-  it 'arujiがなければ保存できないはず' do
-    @user.login_name = 'foo'
-    @user.display_name = 'Foo'
-    @user.password_digest = 'bar'
-    @user.mail_address = 'foo@example.com'
-    @user.save.should be_false
+    it 'passwordを設定するとpassword_digestも設定されるはず' do
+      @user.attributes = {
+          login_name: 'foo3',
+          display_name: 'Foo3',
+          password: 'barboo',
+          mail_address: 'foo@example.com',
+          aruji: true
+      }
+      @user.save.should be_true
+    end
   end
 end
