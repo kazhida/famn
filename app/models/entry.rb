641cd268c1749eed740198fe066c1f7b199d8438
Kaz. HIDA <kazhida@abplus.com>
2012-12-25 10:56:45 +0900

    entryのバリデーションを入れた

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ActiveRecord::Base
   attr_accessible :family_id, :message, :user_id
+
+  validates_presence_of :message
+  validates_presence_of :user_id
+  validates_presence_of :family_id
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,29 @@
+# -*- encoding: utf-8 -*-
+
 require 'spec_helper'
 
-describe Entry do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Entry, 'エントリを追加するとき' do
+
+  before(:each) do
+    # まっさらなユーザを作る
+    @entry = Entry.new
+  end
+
+  describe '妥当性チェック' do
+
+    it 'メッセージがnullではないこと' do
+      @entry.should_not be_valid
+    end
+
+    it 'ユーザIDがnullではないこと' do
+      @entry.message = 'dona dona'
+      @entry.should_not be_valid
+    end
+
+    it '家族IDがnullではないこと' do
+      @entry.message = 'dona dona'
+      @entry.user_id = 1
+      @entry.should_not be_valid
+    end
+  end
 end
