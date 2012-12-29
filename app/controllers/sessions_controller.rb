c592158dfc134945e9fc842f63dd6f02dc2d3693
Kaz. HIDA <kazhida@abplus.com>
2012-12-29 14:39:38 +0900

    sessions_controllerをRESTfulにした。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,32 @@
 class SessionsController < ApplicationController
   skip_before_filter :authenticate_user
 
+  # POST /entries
+  # POST /entries.json
   def create
     user = User.find_by_names(params[:family_name], params[:user_name])
+
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to :root
+      respond_to do |format|
+        format.html {redirect_to :root}
+        format.json { head 201 }
+      end
     else
-      render :new
+      respond_to do |format|
+        format.html { render :new}
+        format.json { head 401 }
+      end
     end
   end
 
+  # DELETE /entries/1
+  # DELETE /entries/1.json
   def destroy
     session.delete :user_id
-    redirect_to [:new, :session]
+    respond_to do |format|
+      format.html { redirect_to [:new, :session] }
+      format.json { head :no_content }
+    end
   end
 end
