0803bd18e02a5c060568349791d929c7221268a5
Kaz. HIDA <kazhida@abplus.com>
2012-12-30 12:49:30 +0900

    単純なViewではないので、自動テストはあきらめて空にした。

--- spec/views/entries/index.html.slim_spec.rb
+++ spec/views/entries/index.html.slim_spec.rb
@@ -1,26 +1,4 @@
 require 'spec_helper'
 
-describe "entries/index" do
-  before(:each) do
-    assign(:entries, [
-      stub_model(Entry,
-        :message => "MyText",
-        :user_id => 1,
-        :family_id => 2
-      ),
-      stub_model(Entry,
-        :message => "MyText",
-        :user_id => 1,
-        :family_id => 2
-      )
-    ])
-  end
-
-  it "renders a list of entries" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-  end
+describe 'entries/index' do
 end
--- spec/views/entries/new.html.slim_spec.rb
+++ spec/views/entries/new.html.slim_spec.rb
@@ -1,22 +1,4 @@
 require 'spec_helper'
 
-describe "entries/new" do
-  before(:each) do
-    assign(:entry, stub_model(Entry,
-      :message => "MyText",
-      :user_id => 1,
-      :family_id => 1
-    ).as_new_record)
-  end
-
-  it "renders new entry form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => entries_path, :method => "post" do
-      assert_select "textarea#entry_message", :name => "entry[message]"
-      assert_select "input#entry_user_id", :name => "entry[user_id]"
-      assert_select "input#entry_family_id", :name => "entry[family_id]"
-    end
-  end
+describe 'entries/new' do
 end
