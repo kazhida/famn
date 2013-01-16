38fc4dc8e69b8f09942d10b9d88cc85ecb14143e
kazhida <kazhida@abplus.com>
2013-01-16 23:13:33 +0900

    メールの設定変えたり、CSSでの半透明パネルをやめたり。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,8 @@ Famn::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'famn.mobi' }
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_url_options = { :host => 'famn.mobi' }
 
   # Enable threaded mode
   # config.threadsafe!
