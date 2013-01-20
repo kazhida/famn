9f97b495bd3ad54466a4c0b2b648bb5a17e68012
kazhida <kazhida@abplus.com>
2013-01-20 22:36:43 +0900

    renderで呼ぶときのフォーマットの指定の仕方を試してみる。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 module ApplicationHelper
+
+  def with_html_format(&block)
+    tmp = formats
+    begin
+      formats = [:html]
+      result = block.call
+    ensure
+      formats = tmp
+    end
+    result
+  end
 end
