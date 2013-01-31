3c3b108879462f3dc7b1300ce04e8bae9f8e04c6
kazhida <kazhida@abplus.com>
2013-01-31 21:27:32 +0900

    jQueryまわりを変えてみた。

--- app/assets/javascripts/mobile_application.js
+++ app/assets/javascripts/mobile_application.js
@@ -5,6 +5,6 @@
  * Time: 11:19
  * To change this template use File | Settings | File Templates.
  */
-//= require jquery
+//= require jquery-1.8.2.min
 //= require jquery_ujs
-//= require jquery.mobile
+//= require jquery.mobile-1.2.0.min
