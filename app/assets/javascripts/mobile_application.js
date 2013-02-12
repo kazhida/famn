124eb86dd1f7fe486347857c1cd2ff6670be0bc9
kazhida <kazhida@abplus.com>
2013-02-12 20:22:28 +0900

    ラジオボタンが、それっぽく表示されるように修正した。また、テーマも少しいじった。

--- app/assets/javascripts/mobile_application.js
+++ app/assets/javascripts/mobile_application.js
@@ -5,6 +5,6 @@
  * Time: 11:19
  * To change this template use File | Settings | File Templates.
  */
-//= require jquery-1.8.2.min
+//= require jquery-1.8.3.min
 //= require jquery_ujs
 //= require jquery.mobile-1.2.0.min
