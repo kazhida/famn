c47a5e154b390a47773e6870bc29e05799d2b057
kazhida <kazhida@abplus.com>
2013-03-02 20:26:09 +0900

    通知メールの送信をスレッド化してみた。

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -7,13 +7,16 @@ class NoticeMailer < ActionMailer::Base
     @entry = entry
     User.by_family_id(entry.family.id).each do |user|
       if user.notice?(entry.destinations)
-        mail(
+        m = mail(
             :to => user.mail_address,
             :subject => "[famn.mobi] #{entry.user.display_name}さんのメッセージ"
         ) do |format|
           format.html
           format.text
-        end.deliver
+        end
+        Thread.new(mail) do |mail|
+          mail.deliver
+        end
       end
     end
   end
