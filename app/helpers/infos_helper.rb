11234d348705f87bfb0bb14946213b16303e07c1
Kaz. HIDA <kazhida@abplus.com>
2013-03-10 14:10:02 +0900

    モバイル版のaboutのリストメニュー化

--- app/helpers/infos_helper.rb
+++ app/helpers/infos_helper.rb
@@ -5,4 +5,8 @@ module InfosHelper
   def about_to(caption, section)
     link_to caption, :controller => :infos, :action => :about, :remote => true, :section => section
   end
+
+  def jump_to(caption, section)
+    sanitize %!<a href="##{section}">#{caption}</a>!
+  end
 end
