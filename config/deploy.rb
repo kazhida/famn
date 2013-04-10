d0b3fa4415b475f1e0613ba55ec27c7ce20b6623
kazhida <kazhida@abplus.com>
2013-04-10 23:04:35 +0900

    デプロイにminaをつかうことにした。

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,74 @@
-set :application, 'famn'
-set :repository,  'git://github.com/kazhida/famn.git'
-set :deploy_to,   '/home/kazhida/rails_app/famn'
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
-set :scm, :git
-set :branch, :master
-set :scm_verbose, true
-set :use_sudo, false
-set :rails_env, 'production'
-set :deploy_via, :remote_cache
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
 
-role :web, 'famn.kazhida.jp'                    # Your HTTP server, Apache/etc
-role :app, 'famn.kazhida.jp'                    # This may be the same as your `Web` server
-role :db,  'famn.kazhida.jp', :primary => true  # This is where Rails migrations will run
+set :domain, 'famn.mobi'
+set :deploy_to, '/var/www/famn'
+set :repository, 'git@github.com:kazhida/famn.git'
+set :branch, 'master'
 
-require    'bundler/capistrano'
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['config/database.yml', 'log']
 
-# rbenv configuration
-default_environment[:RBENV_ROOT] = '$HOME/.rbenv'
-default_environment[:PATH] = '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH'
+# Optional settings:
+set :user, 'cloudn'    # Username in the server to SSH to.
+#   set :port, '30000'     # SSH port number.
 
-set :bundle_flags, '--deployment --quiet --binstubs --shebang ruby-local-exec'
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+  invoke :'rbenv:load'
 
-# DB access
-default_environment[:FAMN_DATABASE_PASSWORD] = ENV['FAMN_DATABASE_PASSWORD']
-default_environment[:FAMN_DATABASE_USERNAME] = ENV['FAMN_DATABASE_USERNAME']
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[-----> Be sure to edit 'shared/config/database.yml'.]
+end
+
+desc 'Deploys the current version to the server.'
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+
+    to :launch do
+      queue 'touch tmp/restart.txt'
+    end
+  end
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
+
